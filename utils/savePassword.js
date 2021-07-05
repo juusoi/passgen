8f15636870f62129a55143534663d7d9c79a694e
Juuso Issakainen <juuso.issakainen@gmail.com>
2021-07-06 02:39:04 +0300

    permission fix

--- utils/savePassword.js
+++ utils/savePassword.js
@@ -4,7 +4,7 @@ const os = require('os')
 const chalk = require('chalk')
 
 const savePassword = (pw) => {
-    fs.open(path.join(__dirname, '../', 'passwords.txt'), 'a', 666, (e, id) => {
+    fs.open(path.join(__dirname, '../', 'passwords.txt'), 'a+', (e, id) => {
         fs.write(id, pw + os.EOL, null, 'utf-8', () =>{
             fs.close(id, () => {
                 console.log(chalk.green('Password saved to passwords.txt'))
